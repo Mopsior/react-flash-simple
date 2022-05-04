3a2681149167fe57b531429331f911c1183f4b0d
Mopsior <80514290+Mopsior@users.noreply.github.com>
2022-05-04 13:55:05 +0200

    Update Flash.js

--- src/Flash.js
+++ src/Flash.js
@@ -7,6 +7,7 @@ gsap.registerPlugin(CSSRulePlugin)
 
 export default function Flash({ title, label, color, theme }) {
     const modal = useRef()
+    const [visible, setVisible] = useState(true)
     const [themeStyle, setTheme] = useState('light');
     useEffect(() => {
         const modalAfter = CSSRulePlugin.getRule('.modal::after')
@@ -20,12 +21,18 @@ export default function Flash({ title, label, color, theme }) {
             .fromTo(modal.current, { x: 300, opacity: 0 }, { duration: 0.5, x: 0, opacity: 1 })
             .fromTo(modalAfter, { width: 0 }, { width: '100%', duration: 3 })
             .to(modal.current, { x: 300, opacity: 0, display: 'none'})
+        .then(() => {
+            setVisible(false)
+        })
     }, [theme, setTheme])
 
     return (
+        visible ?
         <div className='modal' ref={ modal } style={{ "--color": color }} data-mode={ themeStyle }>
             <h1>{ title }</h1>
             <p>{ label }</p>
         </div>
+        :
+        null
     )
 }
